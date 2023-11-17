ea9d213e7e380838cd5c404525c81390cab4a804
spencerwooo <spencer.woo@outlook.com>
2023-11-17 15:54:39 +0800

    refactor main as async

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
-use reqwest;
+use reqwest::Client;
+use url;
 
 const SRUN_PORTAL: &str = "http://10.0.0.55";
 
-fn main() {
-    // http get srun_portal, and get redirect url
-    let client = reqwest::blocking::Client::new();
-    let res = client.get(SRUN_PORTAL).send();
-    if let Err(e) = res {
-        println!("Error: {}", e);
-        return;
-    }
+async fn get_acid(client: &Client) -> String {
+    let resp = client.get(SRUN_PORTAL).send().await.unwrap();
+    let redirect_url = resp.url().to_string();
+    let parsed_url = url::Url::parse(&redirect_url).unwrap();
 
-    let redirect_url = res.unwrap().url().to_string();
-    println!("redirect_url: {}", redirect_url);
+    let mut query = parsed_url.query_pairs().into_owned();
+    return query.find(|(key, _)| key == "ac_id").unwrap().1;
+}
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+    let acid = get_acid(&client).await;
+
+    println!("acid: {}", acid);
 }

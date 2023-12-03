262601a4a46913a05516788ae7315e6fe24b7757
spencerwooo <spencer.woo@outlook.com>
2023-12-03 16:32:25 +0800

    refactor all actions as functions in main

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod xencode;
 use anyhow::Context;
 use anyhow::Result;
 use clap::Parser;
+use cli::ClientArgs;
+use cli::StatusArgs;
 use owo_colors::OwoColorize;
 use owo_colors::Stream::Stderr;
 use owo_colors::Stream::Stdout;
@@ -41,44 +43,7 @@ async fn cli() -> Result<()> {
     match &args.command {
         // check login status
         Some(Commands::Status(status_args)) => {
-            // only verbose on args.verbose = true and not outputting json
-            let login_state = get_login_state(&http_client, args.verbose).await?;
-
-            // output json
-            if status_args.json & !args.verbose {
-                let raw_json = serde_json::to_string(&login_state)?;
-                println!("{}", raw_json);
-                return Ok(());
-            }
-
-            // output human readable
-            match login_state.error.as_str() {
-                "ok" => {
-                    println!(
-                        "{} {} {} is online",
-                        "bitsrun:".if_supports_color(Stdout, |t| t.bright_green()),
-                        &login_state
-                            .online_ip
-                            .to_string()
-                            .if_supports_color(Stdout, |t| t.underline()),
-                        format!("({})", login_state.user_name.clone().unwrap_or_default())
-                            .if_supports_color(Stdout, |t| t.dimmed())
-                    );
-
-                    // print status table
-                    print_login_state(login_state);
-                }
-                _ => {
-                    println!(
-                        "{} {} is offline",
-                        "bitsrun:".if_supports_color(Stdout, |t| t.blue()),
-                        login_state
-                            .online_ip
-                            .to_string()
-                            .if_supports_color(Stdout, |t| t.underline())
-                    );
-                }
-            }
+            srun_status(http_client, status_args, args.verbose).await?
         }
 
         // login or logout
@@ -101,43 +66,15 @@ async fn cli() -> Result<()> {
             )
             .await?;
 
-            if matches!(args.command, Some(Commands::Login(_))) {
-                let resp = srun_client.login(client_args.force, args.verbose).await?;
-                match resp.error.as_str() {
-                    "ok" => println!(
-                        "{} {} {} logged in",
-                        "bitsrun:".if_supports_color(Stdout, |t| t.bright_green()),
-                        resp.online_ip
-                            .to_string()
-                            .if_supports_color(Stdout, |t| t.underline()),
-                        format!("({})", resp.username.clone().unwrap_or_default())
-                            .if_supports_color(Stdout, |t| t.dimmed())
-                    ),
-                    _ => println!(
-                        "{} failed to login, {} {}",
-                        "bitsrun:".if_supports_color(Stdout, |t| t.red()),
-                        resp.error,
-                        format!("({})", resp.error_msg).if_supports_color(Stdout, |t| t.dimmed())
-                    ),
+            match &args.command {
+                Some(Commands::Login(_)) => {
+                    srun_login(&srun_client, client_args, args.verbose).await?
                 }
-            } else if matches!(args.command, Some(Commands::Logout(_))) {
-                let resp = srun_client.logout(client_args.force, args.verbose).await?;
-                match resp.error.as_str() {
-                    "ok" | "logout_ok" => println!(
-                        "{} {} logged out",
-                        "bitsrun:".if_supports_color(Stdout, |t| t.green()),
-                        resp.online_ip
-                            .to_string()
-                            .if_supports_color(Stdout, |t| t.underline())
-                    ),
-                    _ => println!(
-                        "{} failed to logout, {} {}",
-                        "bitsrun:".if_supports_color(Stdout, |t| t.red()),
-                        resp.error,
-                        format!("({})", resp.error_msg).if_supports_color(Stdout, |t| t.dimmed())
-                    ),
+                Some(Commands::Logout(_)) => {
+                    srun_logout(&srun_client, client_args, args.verbose).await?
                 }
-            }
+                _ => {}
+            };
         }
 
         Some(Commands::ConfigPaths) => print_config_paths(),
@@ -145,5 +82,101 @@ async fn cli() -> Result<()> {
         None => {}
     }
 
+    Ok(())
+}
+
+async fn srun_status(
+    http_client: reqwest::Client,
+    status_args: &StatusArgs,
+    verbose: bool,
+) -> Result<()> {
+    // only verbose on args.verbose = true and not outputting json
+    let login_state = get_login_state(&http_client, verbose).await?;
+
+    // output json
+    if status_args.json & !verbose {
+        let raw_json = serde_json::to_string(&login_state)?;
+        println!("{}", raw_json);
+        return Ok(());
+    }
+
+    // output human readable
+    match login_state.error.as_str() {
+        "ok" => {
+            println!(
+                "{} {} {} is online",
+                "bitsrun:".if_supports_color(Stdout, |t| t.bright_green()),
+                &login_state
+                    .online_ip
+                    .to_string()
+                    .if_supports_color(Stdout, |t| t.underline()),
+                format!("({})", login_state.user_name.clone().unwrap_or_default())
+                    .if_supports_color(Stdout, |t| t.dimmed())
+            );
+
+            // print status table
+            print_login_state(login_state);
+        }
+        _ => {
+            println!(
+                "{} {} is offline",
+                "bitsrun:".if_supports_color(Stdout, |t| t.blue()),
+                login_state
+                    .online_ip
+                    .to_string()
+                    .if_supports_color(Stdout, |t| t.underline())
+            );
+        }
+    };
+    Ok(())
+}
+
+async fn srun_login(
+    srun_client: &SrunClient,
+    client_args: &ClientArgs,
+    verbose: bool,
+) -> Result<()> {
+    let resp = srun_client.login(client_args.force, verbose).await?;
+    match resp.error.as_str() {
+        "ok" => println!(
+            "{} {} {} logged in",
+            "bitsrun:".if_supports_color(Stdout, |t| t.bright_green()),
+            resp.online_ip
+                .to_string()
+                .if_supports_color(Stdout, |t| t.underline()),
+            format!("({})", resp.username.clone().unwrap_or_default())
+                .if_supports_color(Stdout, |t| t.dimmed())
+        ),
+        _ => println!(
+            "{} failed to login, {} {}",
+            "bitsrun:".if_supports_color(Stdout, |t| t.red()),
+            resp.error,
+            format!("({})", resp.error_msg).if_supports_color(Stdout, |t| t.dimmed())
+        ),
+    };
+    Ok(())
+}
+
+async fn srun_logout(
+    srun_client: &SrunClient,
+    client_args: &ClientArgs,
+    verbose: bool,
+) -> Result<()> {
+    let resp = srun_client.logout(client_args.force, verbose).await?;
+    match resp.error.as_str() {
+        "ok" | "logout_ok" => println!(
+            "{} {} logged out",
+            "bitsrun:".if_supports_color(Stdout, |t| t.green()),
+            resp.online_ip
+                .to_string()
+                .if_supports_color(Stdout, |t| t.underline())
+        ),
+        _ => println!(
+            "{} failed to logout, {} {}",
+            "bitsrun:".if_supports_color(Stdout, |t| t.red()),
+            resp.error,
+            format!("({})", resp.error_msg).if_supports_color(Stdout, |t| t.dimmed())
+        ),
+    };
     Ok(())
 }
